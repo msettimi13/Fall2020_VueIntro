c77ad8d7aa2c7baa16b41ff9ad10494ec06cad9e
msettimi13 <55293931+msettimi13@users.noreply.github.com>
2020-11-19 15:17:57 -0500

    adding content from class 2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 (() => {
     let vue_vm = new Vue({
-        el: "#app",
+        // el: "#app",
         
         data: {
             message: "Hello from Vue!",
             anotherMessage: "sup",
+            removeAformat: true,
+            showBioData: false,
 
-            collection: [
+            professors: [
                 { name: "Justin", role: "Coordinator", nickname: "nitsuJ" },
                 { name: "John", role: "prof", nickname: "super chill" },
                 { name: "Joe", role: "prof", nickname: "teddy bear" }
             ]
         }, 
+
+        //this is the mounted lifecycle hook. Vue is done creating itself, and has attached itself to the "app" div on the page
+        mounted: function() {
+            console.log("Vue is mounted!");
+            //alert("hi would ya")
+
+            this.professors.push({name: "Jarrod", role: "supermodel prof", nickname: "zoolander"})
+        },
+
+        //run a method when we change our view (update the DOM with vue)
+        updated: function() {
+            console.log('Vue just updated the DOM');
+        },
 
         methods: {
             logClicked() {
@@ -20,6 +35,13 @@
 
             clickHeader() {
                 console.log("clicked on header");
+            },
+
+            removeProf(target) {
+                // remove this prof from the professors array
+                console.log('clicked to remove prof', target, target.name);
+                // the "this" keyword inside a vue instance REFERS to the vue instance itself by default
+                this.showBioData = this.showBioData ? false: true
             }
         }
     }).$mount("#app");
